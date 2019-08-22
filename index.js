e454c6f29ca4ea2c20717e33b453c82a88981c6e
sommazum <somabrata.somu@gmail.com>
2019-08-22 11:41:06 +0530

    dev mode debugging

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./dist/react-portal-toaster.js");
 } else {
-  module.exports = require("./dist/react-portal-toaster.js");
+  module.exports = require("./src/index");
 }
